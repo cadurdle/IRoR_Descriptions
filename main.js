2bd6d19fd1e6ca966e15d1bf234131d688b49aa6
Courtney Durdle <cadurdle@ucsb.edu>
2024-05-23 14:51:56 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const API_KEY = 'AIzaSyCEGEi3s9QcvzfPqAKRh3z8Vp3rTzQ-zZk';
 const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
 const SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
 
